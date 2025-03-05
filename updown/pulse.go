a13c0e056203d5e5482e8721d3e58cdee17b0619
Anton Novojilov <andy@essentialkaos.com>
2025-03-06 01:16:26 +0300

    Add updown pulse support

---
+++ updown/pulse.go
@@ -0,0 +1,49 @@
+package updown
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+//                                                                                    //
+//                         Copyright (c) 2025 ESSENTIAL KAOS                          //
+//                       PRIVATE SOFTWARE, ALL RIGHTS RESERVED                        //
+//                                                                                    //
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/essentialkaos/ek/v13/req"
+)
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+// Webhook is updown pulse URL
+var Webhook string
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+// Pulse sends request to Updown.io pulse
+func Pulse(payload string) error {
+	if Webhook == "" {
+		return nil
+	}
+
+	rt := req.NewRetrier()
+	r := req.Request{URL: Webhook, Method: req.GET, AutoDiscard: true}
+
+	if payload != "" {
+		r.Method = req.POST
+		r.Body = payload
+	}
+
+	_, err := rt.Do(r, req.Retry{
+		Num:    5,
+		Pause:  time.Second / 2,
+		Status: req.STATUS_OK,
+	})
+
+	if err != nil {
+		return fmt.Errorf("Can't send request to updown pulse URL: %v", err)
+	}
+
+	return nil
+}
